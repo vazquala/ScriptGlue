8e4b27da90878b772bf305046990ef55ab41fdd0
vazquala000 <vazquala000@warren.k12.in.us>
2026-01-29 11:01:09 -0500

    Initial commit

--- app.js
+++ app.js
@@ -14,6 +14,21 @@ nameSpan.innerText = name;
 let luckyInput = prompt("Pick a lucky number (0-100)");
 let luckyNumber = parseInt(luckyInput);
 
+function fixLucky(lucky) {
+    if (isNaN(lucky)) {
+        return "You don't have any lucky number.";
+    } else {
+        return lucky.toString();
+    }
+}
+
+let luckyOutput = fixLucky(luckyNumber);
+
 const luckyNumberSpan = document.getElementById("luckyNumber");
 luckyNumberSpan.innerText = luckyNumber.toString();
+
+const facts = [name, luckyOutput];
+
+const statusMessageSpan = document.getElementById("statusMessage");
+statusMessageSpan.innerText = facts.toString()
 
